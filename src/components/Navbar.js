a24d4ff6851749536ea3ddc32fff375e3a37611b
foliaco <foliaco18@gmail.com>
2022-04-04 22:31:09 -0500

    first git

---
+++ src/components/Navbar.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Navbar = props => {
+  return (
+    <nav>
+        <ul>
+            <li><a href='#'>Inicio</a></li>
+            <li><a href='#'>Productos</a></li>
+            <li><a href='#'>Contactos</a></li>
+        </ul>
+    </nav>
+  )
+}
+
+
+export default Navbar
---
+++ src/pages/Home.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import Navbar from '../components/Navbar';
+import '../styles/Homes.css';
+export const Home = () => {
+  return (
+    <main>
+        <header className='main-header'>
+            <div className='left'>
+                <h1>CONGELADOS Y PASABOCAS <br/>MANZUR</h1>
+            </div>
+            <div className='right'>
+                <Navbar/>
+            </div>
+        </header>
+        <section>
+                a
+        </section>
+    </main>
+  )
+}

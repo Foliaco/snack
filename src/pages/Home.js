acce3c1982d09e1d98756ded2b7cca188d5160dd
Foliaco <foliaco18@gmail.com>
2022-04-06 18:23:39 -0500

    header

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,12 +2,20 @@ import React from 'react'
 import Navbar from '../components/Navbar';
 import {HiShoppingCart} from 'react-icons/hi'
 import '../styles/Homes.css';
+import img from '../images/ropa-vieja-empanadas.png';
 export const Home = () => {
     return ( 
       <main>
         <Navbar/>
-        <header>
+        <header className='header-main'>
             <h6><HiShoppingCart/>Barrio el socorro  m-57 l-16 plan 282 </h6>
+            <h1>CONGELADOS Y PASABOCAS <span>MANZUR</span></h1>
+            <div className='grid-cont'>
+              <div class='left-absolute'>
+                <img src={img} id='img-empanadas' alt='Empanadas' />
+              </div>
+            </div>
+            <button style={{margin:'40px 70px'}}><span>Nuestros Productos</span></button>
         </header>
       </main>
     )
